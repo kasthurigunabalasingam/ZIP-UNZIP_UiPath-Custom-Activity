2c5994f124cae3b11be55ca3762f3443cbb6ae7a
Kasthuri <gunabalasingam.kasthuri@cse.metalanka.com>
2018-08-29 11:04:54 +0530

    Initial Commit

---
+++ ZipUnZipActivities-Source File/ZipUnZipActivities/UnZipActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using Ionic.Zip;
+using System.IO;
+
+namespace ZipUnZipActivities
+{
+    public class UnZipActivity : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("Zip File")]
+        [Description("The full path of ZipFile")]
+        public InArgument<string> ZipFile { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("Extract Folder Path")]
+        [Description("The full path of output folder location")]
+        public InArgument<string> ExtractLocation { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("Password")]
+        [Description("Password")]
+        public InArgument<string> Password { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var SourceFilePath = ZipFile.Get(context);
+            var DestinationFolderPath = ExtractLocation.Get(context);
+            var Password_Val = Password.Get(context);
+
+            using (ZipFile zip = new ZipFile(SourceFilePath))
+            {
+                zip.Password = Password_Val.ToString();
+                zip.ExtractAll(DestinationFolderPath);
+            }
+        }
+    }
+}
---
+++ ZipUnZipActivities-Source File/ZipUnZipActivities/ZipActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using Ionic.Zip;
+using System.IO;
+
+namespace ZipUnZipActivities
+{
+    public class ZipActivity : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("Folder Path")]
+        [Description("The full path of selected folder")]
+        public InArgument<string> FolderPath { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("Zipped Path")]
+        [Description("The full path of output zip file")]
+        public InArgument<string> ZippedPath { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [DisplayName("Password")]
+        [Description("Password")]
+        public InArgument<string> Password { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var SourceFilePath = FolderPath.Get(context);
+            var DistinationZipPath = ZippedPath.Get(context);
+            var Password_Val = Password.Get(context);
+
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.Password = Password_Val.ToString();
+                zip.AddDirectory(SourceFilePath);
+                zip.Save(DistinationZipPath);
+            }
+        }
+    }
+}
+
